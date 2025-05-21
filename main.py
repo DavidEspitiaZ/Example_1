c65d8966a36f2d0c2cc8f3c3d9c24e2450446eb4
David Espitia Zapata <117323112+DavidEspitiaZ@users.noreply.github.com>
2025-05-21 18:15:34 -0500

    feat: pipeline

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hola mundo Prueba :D")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+#api web que nos permite conservar con el servidor
+#estado de la aplicacion
+@app.get("/")
+def hello():
+    return {"message": "Hello World"}
